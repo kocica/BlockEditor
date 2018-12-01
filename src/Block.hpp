346327cacd332d30bc094d394422bbd873929e56
Filip Kočica <filda1997@gmail.com>
2018-12-01 11:22:12 +0100

    Minor fixes

--- src/Block.hpp
+++ src/Block.hpp
@@ -34,36 +34,36 @@ namespace BlockEditorLogic
 		friend
 		std::ostream& 	operator<<(std::ostream&, const CBlock&);
 
-		CPort* 					addOutputPort(ID portID);
-		void 						addInputPort(Ports whichPort, CPort* port);
-		void 						removePort(Ports whichPort);
-		void 						setPort(Ports whichPort, void *ptr = nullptr);
-		bool 						hasPort(Ports whichPort) const;
-		CPort* 					getPort(Ports whichPort) const;
+		CPort*   addOutputPort(ID portID);
+		void     addInputPort(Ports whichPort, CPort* port);
+		void     removePort(Ports whichPort);
+		void     setPort(Ports whichPort, void *ptr = nullptr);
+		bool     hasPort(Ports whichPort) const;
+		CPort*   getPort(Ports whichPort) const;
 
 		virtual
-		PortValue 			performOperation(PortValue&& pv1, PortValue&& pv2);
+		PortValue   performOperation(PortValue&& pv1, PortValue&& pv2);
 
-		void 						setInputValue(PortValue);
-		PortValue 			getValue() const;
-		TypeName	 			getTypeName() const;
-		ID 							getPortID(Ports whichPort) const;
-		ID 							getID() const;
-		EBlockType 			getType() const;
+		void        setInputValue(PortValue);
+		PortValue   getValue() const;
+		TypeName    getTypeName() const;
+		ID          getPortID(Ports whichPort) const;
+		ID          getID() const;
+		EBlockType  getType() const;
 		std::pair<int, int> getPosition() const;
-		void  					setPosition(std::pair<int, int>);
+		void        setPosition(std::pair<int, int>);
 
 	protected:
-		ID     									m_blockID;       /**< Unique ID */
-		EBlockType		    			m_bt;			       /**< Type of block {ADD|SUB|etc.} */
-		int              				m_x, m_y;        /**< Position of block in scheme */
-		PortValue								m_value = .0;    /**< Value of input port */
+		ID          m_blockID;         /**< Unique ID */
+		EBlockType  m_bt;              /**< Type of block {ADD|SUB|etc.} */
+		int         m_x, m_y;          /**< Position of block in scheme */
+		PortValue   m_value = .0;      /**< Value of input port */
 
-		CPort 									*m_inputPort1;
-		CPort 									*m_inputPort2;    /**< Input ports */
-		CPort 									*m_outputPort;    /**< Output port */
+		CPort       *m_inputPort1;
+		CPort       *m_inputPort2;     /**< Input ports */
+		CPort       *m_outputPort;     /**< Output port */
 
-		TypeName 								m_name;					  /**< Name of type of this block */
+		TypeName    m_name;            /**< Name of type of this block */
 	private:
 
 	};
